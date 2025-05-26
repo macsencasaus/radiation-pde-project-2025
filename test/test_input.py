e27baaf383db818c79910e042fd5e9c7c5b8c3ff
Macsen Casaus <macsencasaus@gmail.com>
2025-05-26 13:04:39 -0500

    unit tests

--- test/test_input.py
+++ test/test_input.py
@@ -8,3 +8,19 @@ class TestInputData(unittest.TestCase):
     def test_constructor(self):
         input_dir = "test/test-inputs/input1.json"
         _ = InputData(input_dir)
+
+    def test_values(self):
+        expected = {
+            "n_zones": 5,
+            "n_cells": [1, 2, 3, 4, 5],
+            "zone_length": [1, 2, 3, 4, 5],
+            "sigma_t": [3, 4, 5, 6, 7],
+            "source": [9, 8, 7, 6, 5],
+            "boundary_values": (1, 2),
+        }
+
+        input_dir = "test/test-inputs/input1.json"
+        inp = InputData(input_dir)
+
+        for key, v in expected.items():
+            self.assertEqual(getattr(inp, key), v, key)
--- test/test_mesh.py
+++ test/test_mesh.py
@@ -11,3 +11,55 @@ class TestInputData(unittest.TestCase):
         inp = InputData(input_dir)
 
         _ = Mesh(inp)
+
+    def test_values(self):
+        expected = {
+            "n_points": 16,
+            "n_cells": 15,
+            "length": 15,
+            "cells": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+            "gridpoints": [
+                0.0,
+                1.0,
+                2.0,
+                3.0,
+                4.0,
+                5.0,
+                6.0,
+                7.0,
+                8.0,
+                9.0,
+                10.0,
+                11.0,
+                12.0,
+                13.0,
+                14.0,
+                15.0,
+            ],
+            "h": [
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+            ],
+            "mat_id": [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+        }
+
+        input_dir = "test/test-inputs/input1.json"
+        inp = InputData(input_dir)
+
+        m = Mesh(inp)
+
+        for key, v in expected.items():
+            self.assertEqual(getattr(m, key), v, key)

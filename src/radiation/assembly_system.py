189b5a92be9d891175aa81f2f57876f6e26fbf8b
Macsen Casaus <macsencasaus@gmail.com>
2025-05-26 15:43:16 -0500

    generate sparse matrix

--- src/radiation/assembly_system.py
+++ src/radiation/assembly_system.py
@@ -1,6 +1,7 @@
 from radiation.mesh import Mesh
 import numpy as np
 from radiation.input_data import InputData
+from scipy.sparse import csr_matrix
 
 def assemble_source(alpha: float, mu: float, mesh: Mesh, data: InputData) -> np.ndarray:
     bs = np.zeros(mesh.n_points)
@@ -17,3 +18,75 @@ def assemble_source(alpha: float, mu: float, mesh: Mesh, data: InputData) -> np.
         bs[-1] = mesh.h[-1] * data.source[-1] / 2
     return bs
 
+def generate_sparse_pattern(mesh: Mesh) -> tuple[np.ndarray, np.ndarray]:
+    rows = np.zeros(3 * (mesh.n_points) - 2) 
+    cols = np.zeros(3 * (mesh.n_points) - 2) 
+    rowsIdx, colsIdx = 0 , 0
+    
+    for i in range(mesh.n_points):
+        if i == 0:
+            rows[rowsIdx + 0] = i
+            rows[rowsIdx + 1] = i
+
+            cols[colsIdx + 0] = i
+            cols[colsIdx + 1] = i + 1
+
+            colsIdx += 2
+            rowsIdx += 2
+        elif i == mesh.n_points - 1:
+            rows[rowsIdx + 0] = i
+            rows[rowsIdx + 1] = i
+
+            cols[colsIdx + 0] = i - 1
+            cols[colsIdx + 1] = i
+
+            colsIdx += 2
+            rowsIdx += 2
+        else:
+            rows[rowsIdx + 0] = i
+            rows[rowsIdx + 1] = i
+            rows[rowsIdx + 2] = i
+
+            cols[colsIdx + 0] = i - 1
+            cols[colsIdx + 1] = i
+            cols[colsIdx + 2] = i + 1
+
+            colsIdx += 3
+            rowsIdx += 3
+
+    return rows, cols
+    
+def assemble_transport_matrix(mu: float, mesh: Mesh, data: InputData) -> csr_matrix:
+    rows, cols = generate_sparse_pattern(mesh)
+
+    matrix_data = np.zeros(3 * (mesh.n_points) - 2) 
+    dataIdx = 0
+    for i in range(mesh.n_points):
+        if i == 0:
+            matrix_data[dataIdx] = -mu / 2 + (1 / 3) * data.sigma_t[0] * mesh.h[0]
+            matrix_data[dataIdx + 1] = -mu/2 + data.sigma_t[0] * mesh.h[0]/6
+            dataIdx += 2
+            pass
+        elif i == mesh.n_points - 1:
+            matrix_data[dataIdx] = -mu / 2 + data.sigma_t[-1] * mesh.h[-1] / 6
+            matrix_data[dataIdx + 1] = mu / 2 + (1 / 3) * data.sigma_t[-1] * mesh.h[-1]
+            pass
+        else:
+            # i == j - 1
+            matrix_data[dataIdx] = -mu / 2 + data.sigma_t[mesh.mat_id[i]] * mesh.h[i] / 6
+            # i == j
+            matrix_data[dataIdx + 1] = (1/3) * (mesh.h[i] * data.sigma_t[mesh.mat_id[i]] + mesh.h[i-1] * data.sigma_t[mesh.mat_id[i - 1]])
+            # i == j + 1
+            matrix_data[dataIdx + 2] = -mu/2 + data.sigma_t[mesh.mat_id[i]] * mesh.h[i]/6
+            dataIdx += 3
+    
+    # NOTE: strong enforcement of boundary conditions
+    if mu > 0:
+        matrix_data[0] = 1
+        matrix_data[1] = 0
+    elif mu < 0:
+        matrix_data[-1] = 1
+        matrix_data[-2] = 0
+    
+    matrix = csr_matrix((matrix_data, (rows, cols)), shape = (mesh.n_points, mesh.n_points))
+    return matrix
--- test/test_assembly.py
+++ test/test_assembly.py
@@ -1,6 +1,10 @@
 import unittest
 
-from radiation.assembly_system import assemble_source
+import numpy as np
+
+from scipy.sparse import csr_matrix
+
+from radiation.assembly_system import assemble_source, generate_sparse_pattern, assemble_transport_matrix
 from radiation.input_data import InputData
 from radiation.mesh import Mesh
 
@@ -16,3 +20,26 @@ class TestAssemblySystem(unittest.TestCase):
         bs = assemble_source(inp.boundary_values[0], 1, m, inp)
 
         print(bs)
+
+    def test_generate_sparce(self):
+        input_dir = "test/test-inputs/input2.json"
+
+        inp = InputData(input_dir)
+        m = Mesh(inp)
+
+        rows, cols = generate_sparse_pattern(m)
+
+        data = np.ones(3 * m.n_points - 2)
+
+        sparse_mat = csr_matrix((data, (rows, cols)), shape=(m.n_points, m.n_points))
+
+        print(sparse_mat.toarray())
+
+    def test_assemble_matrix(self):
+        input_dir = "test/test-inputs/input2.json"
+
+        inp = InputData(input_dir)
+        m = Mesh(inp)
+
+        matrix = assemble_transport_matrix(1, m, inp)
+        print(matrix.toarray())

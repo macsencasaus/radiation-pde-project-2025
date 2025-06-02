c26a95c4253e5337dc760449d868dc352a07e994
Macsen Casaus <macsencasaus@gmail.com>
2025-06-02 10:11:45 -0500

    remove tqdm from dsa

--- src/dsa.py
+++ src/dsa.py
@@ -6,7 +6,6 @@ from src.input_data import InputData
 from src.fixed_point import compute_initial_guess, compute_initial_guess, compute_psi_star, compute_delta
 import numpy as np
 from scipy.sparse.linalg import spsolve
-from tqdm import tqdm
 
 
 def diffusion_synthetic_acceleration(n_angles: int, mesh: Mesh, data: InputData, tol: float, max_iter: int) -> tuple[np.ndarray, np.ndarray, float, float]:
@@ -21,7 +20,7 @@ def diffusion_synthetic_acceleration(n_angles: int, mesh: Mesh, data: InputData,
     phi_n = start_phi
     error_scale = np.linalg.norm(phi_n, 1) 
     while relative_error > tol and iter < max_iter:
-        for l, angle in tqdm(enumerate(angles)):
+        for l, angle in enumerate(angles):
             A = As[l]
             b = assemble_scattered_source(angle, mesh, data, phi_n)
             psi_zero[l] = spsolve(A, b, permc_spec=None, use_umfpack=True)
